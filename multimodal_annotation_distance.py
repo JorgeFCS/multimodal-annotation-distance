488233a9db93a98d390ff73d19dd557f11e1d886
Jorge Ciprián <ciprianjorge.16@gmail.com>
2023-09-11 16:28:17 +0200

    Time range functionality complete

--- multimodal_annotation_distance.py
+++ multimodal_annotation_distance.py
@@ -4,8 +4,11 @@ Based on the R code originally proposed by Camila Barros and
 
 """
 
+from typing import List
+
 import pandas as pd
 import pympi
+from tqdm import tqdm
 
 __author__ = "Jorge Ciprian"
 __credits__ = ["Jorge Ciprian", "Camila Barros"]
@@ -13,40 +16,99 @@ __license__ = ""  # TBD
 __version__ = "0.1.0"
 __status__ = "Development"
 
-def main():
-    get_overlap()
 
-def get_overlap():
-    file_path = "./ELAN-data/bgest_001.eaf"
+def main():
+    file_paths = ["./ELAN-data/bgest_001.eaf"]
     search_value = "stroke"
     ref_tier = "GE-Phase"
     comparison_tiers = ["GE-Phrase", "NTB"]
+    get_overlaps(file_paths, ref_tier, search_value, comparison_tiers)
 
-    # Reading EALN file.
-    eaf = pympi.Elan.Eaf(file_path)
-    results_df = pd.DataFrame([], columns=['Begin_Time_ms',
-                                           'End_Time_ms',
-                                           'GE-Phrase',
-                                           'GE-Phase'])
-    # Get the elements in ref_tier that match the search_value.
-    ref_search_intervals = []
-    for annotation in eaf.get_annotation_data_for_tier(ref_tier):
-        if search_value in annotation:
-            ref_search_intervals.append(annotation)
-    
-    for annotation in ref_search_intervals:
-        print("Reference interval times for ", annotation[2] ,": ", annotation[0], " - ", annotation[1])
-        #print("Found matches:")
-        for tier in comparison_tiers:
-            potential_matches = eaf.get_annotation_data_between_times(tier,
-                                                                    annotation[0],
-                                                                    annotation[1])
-            # Validate that it is not the immediate previous - next entry.
-            for potential_match in potential_matches:
-                if not annotation[0] == potential_match[1] and not annotation[1] == potential_match[0]:
-                    print("Found matches:")
-                    print(potential_match)
-            
+
+def construct_columns(ref_tier: str,
+                      search_value: str) -> List[str]:
+    """
+    Constructs the column names for the results dataframe.
+
+    :param ref_tier: Reference tier against which the `comparison_tiers`
+        will be compared.
+    :type ref_tier: string
+    :param search_value: Particular value of `ref_tier` for which to search
+        for overlaps.
+    :type search_value: string
+
+    :return: List containing the column names for the results dataframe.
+    :rtype: List[string]
+    """
+    # Base column names.
+    columns = [ref_tier + "_" + search_value + "_start_ms",
+               ref_tier + "_" + search_value + "_end_ms",
+               ref_tier + "_" + search_value + "_duration"]
+    # Column names for the different `comparison tiers`.
+    columns.extend(["Tier", "Value", "Begin_ms", "End_ms",
+                    "Duration", "Overlap_time", "Overlap_ratio",
+                    "Diff_start", "Diff_end"])
+    return columns
+
+
+def get_overlaps(file_paths: List[str],
+                 ref_tier: str,
+                 search_value: str,
+                 comparison_tiers: List[str]) -> pd.DataFrame:
+    """
+    Function that searches for overlaps between a given `ref_value` in 
+    a reference tier `ref_tier` and one or more `comparison_tiers`. Computes additional
+    data such as overlap ratio, overlap time, etc., and returns the results in tabular format.
+
+    :param file_paths: List of EAF files for analysis.
+    :type file_paths: List[string]
+    :param ref_tier: Reference tier against which the `comparison_tiers`
+        will be compared.
+    :type ref_tier: string
+    :param search_value: Particular value of `ref_tier` for which to search
+        for overlaps.
+    :type search_value: string
+    :param comparison_tiers: List of tiers to compare against `ref_tier`.
+    :type comparison_tiers: List[string]
+
+    :return: Dataframe containing the computed results in tabular format.
+    :rtype: pandas Dataframe
+    """
+    # Constructing columns
+    columns = construct_columns(ref_tier, search_value)
+    # Initializing results dataframe.
+    results_df = pd.DataFrame([], columns=columns)
+
+    for file_path in tqdm(file_paths):
+        # Reading EALN file.
+        eaf = pympi.Elan.Eaf(file_path)
+        # Get the elements in ref_tier that match the search_value.
+        ref_search_intervals = []
+        for annotation in eaf.get_annotation_data_for_tier(ref_tier):
+            if search_value in annotation:
+                ref_search_intervals.append(annotation)
+
+        for annotation in ref_search_intervals:
+            row_base = [annotation[0], annotation[1], annotation[1] - annotation[0]]
+            for tier in comparison_tiers:
+                potential_matches = eaf.get_annotation_data_between_times(tier,
+                                                                        annotation[0],
+                                                                        annotation[1])
+                # Validate that it is not the immediate previous - next entry.
+                for match in potential_matches:
+                    if not annotation[0] == match[1] and not annotation[1] == match[0]:
+                        # Computing overlap data.
+                        duration = match[1] - match[0]
+                        overlap_time = min(annotation[1], match[1]) - max(annotation[0], match[0])
+                        overlap_ratio = round(overlap_time / (annotation[1] - annotation[0]), 3)
+                        row_part = [tier, match[2], match[0], match[1],
+                                    duration, overlap_time, overlap_ratio,
+                                    annotation[0] - match[0],
+                                    annotation[1] - match[1]]
+                        row = row_base + row_part
+                        results_df.loc[len(results_df.index)] = row
+
+    results_df.to_csv("./results.csv", index=False, encoding="utf-8-sig") 
 
 
 def test_experiments():
